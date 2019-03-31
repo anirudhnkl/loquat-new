f0cb1f46e8fd95b89cecd5bb4e14b5891eac5188
tambulkar <tanish.ambulkar99@gmail.com>
2019-03-30 20:37:08 -0700

    Created User Endpoint

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -14,17 +14,56 @@
 
 # [START gae_python37_app]
 from flask import Flask
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from iexfinance.stocks import Stock
+import json
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+# Use a service account
+cred = credentials.Certificate("Loquat-a526045a3f62.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
+
+# https://loquat.appspot.com
+@app.route('/user/<user_id>')
+def show_user_profile(user_id):
+    # show the user profile for that user
+    doc_ref = db.collection(u'users').document(user_id)
+    try:
+        doc = doc_ref.get()
+        portfolio_name = doc.to_dict()['portfolio']
+        print('Portfolio:'+ portfolio_name)
+        stocks = db.collection(u'portfolio').document(portfolio_name).get().to_dict()['portfolio']
+
+        portfolioList = []
+        stockObj = {}
+        for stock, quantity in stocks.items():
+            a = Stock(stock, token="pk_4552c3e208aa495ea2803d07e1b2feb0")
+            stockObj['symbol'] = stock
+            stockObj['price'] = a.get_price()
+            stockObj['quantity'] = quantity
+            portfolioList.append(json.dumps(stockObj))
+    except:
+        print(u'No such document!')
+        return "ERROR"
+
+    return str(portfolioList)
+
+@app.route('/group/<group_id>')
+def show_post(group_id):
+    # show the post with the given id, the id is an integer
+    return 'Group %s' % group_id
 
 
 if __name__ == '__main__':
